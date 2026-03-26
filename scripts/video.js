8fc8bd1dd154fb458d8a505f2bc28adff0b96541
lumedev12 <lumedev12@gmail.com>
2026-03-26 18:37:05 -0300

    feat/skip button

--- scripts/video.js
+++ scripts/video.js
@@ -1,12 +1,20 @@
 const video = document.getElementById("introVideo");
 const nextBtn = document.getElementById("nextBtn");
+const skipBtn = document.getElementById("skipBtn");
 
 // Acelera o vídeo
 video.playbackRate = 1.25;
 
 video.onended = function(){
+    skipBtn.style.display = "none";
     nextBtn.style.display = "inline-block";
 };
+
+function skipVideo(){
+    video.currentTime = video.duration;
+    nextBtn.style.display = "inline-block";
+    skipBtn.style.display = "none";
+}
 
 function next(){
     window.location.href = "/ratinho_beta/"
